5a724d94056aa7c9e9f01a2a2ed3ff3375f86339
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-04-19 19:38:22 -0700

    fixing syntax errors in links

--- js/header.js
+++ js/header.js
@@ -8,13 +8,13 @@ class Header extends HTMLElement {
             <header>
                 <p> Hannah Del Real </p>
                 <nav class="nav-links">
-                    <a href="/index.html" id="home">Home</a>
-                    <a href="/about.html" id="about">About</a>
-                    <a href="resume.html" id="resume">Resume</a>
-                    <a href="/projects.html" id="projects">Projects</a>
-                    <a href="/db-diagrams.html" id="db-diagrams">Database Diagrams</a>
-                    <a href="/api-unit-test.html" id="api-unit-test">API Unit tests</a>
-                    <a href="/bucket-list.html" id="bucket-list">Bucket List</a>
+                    <a href="./index.html" id="home">Home</a>
+                    <a href="./about.html" id="about">About</a>
+                    <a href="./resume.html" id="resume">Resume</a>
+                    <a href="./projects.html" id="projects">Projects</a>
+                    <a href="./db-diagrams.html" id="db-diagrams">Database Diagrams</a>
+                    <a href="./api-unit-test.html" id="api-unit-test">API Unit tests</a>
+                    <a href="./bucket-list.html" id="bucket-list">Bucket List</a>
                 </nav>
             </header>`;
     }
