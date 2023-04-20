68f3aa2ec6faf1bc3886f6038ead43ae21e0feb4
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-04-19 19:21:41 -0700

    adding files created for pages of portfolio

---
+++ js/footer.js
@@ -0,0 +1,22 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super();   
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="footer">
+                <div class="links">
+                    <a href="https://github.com/Hannahd06?tab=repositories">GitHub Repositories</a>
+                    <a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/">Bellevue University's Web Development Degree</a>
+                    <a href="https://www.youtube.com/@bellevueuniversity">Bellevue University Youtube Channel</a>
+                    <a href="https://github.com/buwebdev">Bellevue University GitHub</a>
+                </div>
+            </div>`; 
+    }
+}
+
+customElements.define("page-footer", Footer);
+
+
+                
