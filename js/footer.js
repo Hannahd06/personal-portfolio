d246e321ce16660e7ca85225bcfb1702dd987f51
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-04-25 12:44:43 -0700

    modified links

--- js/footer.js
+++ js/footer.js
@@ -8,9 +8,9 @@ class Footer extends HTMLElement {
             <div class="footer">
                 <div class="links">
                     <a href="https://github.com/Hannahd06?tab=repositories" target="blank">GitHub Repositories</a>
-                    <a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/">Bellevue University's Web Development Degree</a>
-                    <a href="https://www.youtube.com/@bellevueuniversity">Bellevue University Youtube Channel</a>
-                    <a href="https://github.com/buwebdev">Bellevue University GitHub</a>
+                    <a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/" target="blank">Bellevue University's Web Development Degree</a>
+                    <a href="https://www.youtube.com/@bellevueuniversity" target="blank">Bellevue University Youtube Channel</a>
+                    <a href="https://github.com/buwebdev" target="blank>Bellevue University GitHub</a>
                 </div>
             </div>`; 
     }
