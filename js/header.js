81bb9003641d22e313af9295ca3f314f06ec45e7
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-08-10 21:00:43 -0700

    fixing syntax

--- js/header.js
+++ js/header.js
@@ -8,18 +8,18 @@ class Header extends HTMLElement {
             <header>
                 <p> Hannah Del Real </p>
                 <nav class="nav-links">
-                    <a href="../index.html" id="home">Home</a>
-                    <a href="../about.html" id="about">About</a>
-                    <a href="../resume.html" id="resume">Resume</a>
-                    <a href="../projects.html" id="projects">Projects</a>
-                    <a href="../db-diagrams.html" id="db-diagrams">Database Diagrams</a>
-                    <a href="../api-unit-test.html" id="api-unit-test">API Unit tests</a>
-                    <a href="../bucket-list.html" id="bucket-list">Bucket List</a>
+                    <a href="./index.html" id="home">Home</a>
+                    <a href="./about.html" id="about">About</a>
+                    <a href="./resume.html" id="resume">Resume</a>
+                    <a href="./projects.html" id="projects">Projects</a>
+                    <a href="./db-diagrams.html" id="db-diagrams">Database Diagrams</a>
+                    <a href="./api-unit-test.html" id="api-unit-test">API Unit tests</a>
+                    <a href="./bucket-list.html" id="bucket-list">Bucket List</a>
                     <div class="dropdown">
                         <button class="dropbtn">DevOps <i class="fa fa-caret-down"></i>
                         </button>
                         <div class="dropdown-content">
-                            <a href="web-430/delreal-tech-value-stream.html">Tech Value Stream</a>
+                            <a href="./web-430/delreal-tech-value-stream.html">Tech Value Stream</a>
                             <a href="#">Presentation 2</a>
                          </div>
                     </div>     
