222d810796cfe1ff127c9159b0e1a83a00797e32
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-09-08 19:33:08 -0700

    adding files for presentation 5

--- js/header.js
+++ js/header.js
@@ -8,13 +8,13 @@ class Header extends HTMLElement {
             <header>
                 <p> Hannah Del Real </p>
                 <nav class="nav-links">
-                    <a href="./index.html" id="home">Home</a>
-                    <a href="./about.html" id="about">About</a>
-                    <a href="./resume.html" id="resume">Resume</a>
-                    <a href="./projects.html" id="projects">Projects</a>
-                    <a href="./db-diagrams.html" id="db-diagrams">Database Diagrams</a>
-                    <a href="./api-unit-test.html" id="api-unit-test">API Unit tests</a>
-                    <a href="./bucket-list.html" id="bucket-list">Bucket List</a>
+                <a href="https://hannahd06.github.io/personal-portfolio/index.html" id="home">Home</a>
+                <a href="https://hannahd06.github.io/personal-portfolio/about.html" id="about">About</a>
+                <a href="https://hannahd06.github.io/personal-portfolio/resume.html" id="resume">Resume</a>
+                <a href="https://hannahd06.github.io/personal-portfolio/projects.html" id="projects">Projects</a>
+                <a href="https://hannahd06.github.io/personal-portfolio/db-diagrams.html" id="db-diagrams">Database Diagrams</a>
+                <a href="https://hannahd06.github.io/personal-portfolio/api-unit-test.html" id="api-unit-test">API Unit tests</a>
+                <a href="https://hannahd06.github.io/personal-portfolio/bucket-list.html" id="bucket-list">Bucket List</a>
                     <div class="dropdown">
                         <button class="dropbtn">DevOps <i class="fa fa-caret-down"></i>
                         </button>
@@ -23,7 +23,9 @@ class Header extends HTMLElement {
                             <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-two-pizza-team-rule.html">The Two-Pizza Team Rule</a>
                             <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-automated-testing.html">DevOps Automated Testing</a>                            
                             <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-continuous-integration.html">Continuous Integration</a>                            
-                            <a href="#">Presentation 5</a>
+                            <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-pager-rotation-duties.html">Pager Rotation Duties</a>                            
+                            <a href="#">Presentation 6</a>
+                            
                         <div>
                     </div>     
                 </nav>
