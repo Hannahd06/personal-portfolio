639df1b4d0aec39f1dadcbda26126dd2fda1759a
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-09-22 16:48:01 -0700

    adding files for presentation 7

--- js/header.js
+++ js/header.js
@@ -25,7 +25,8 @@ class Header extends HTMLElement {
                             <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-continuous-integration.html">Continuous Integration</a>                            
                             <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-pager-rotation-duties.html">Pager Rotation Duties</a>                            
                             <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-change-approval.html">Dangers of Change Approval</a>                            
-                            <a href="#">Presentation 7</a>
+                            <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-technology-vs-business.html">Dangers of Change Approval</a>                            
+                            <a href="#">Presentation 8</a>
                             
                         <div>
                     </div>     
