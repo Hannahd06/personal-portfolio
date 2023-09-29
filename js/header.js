dfd95023ed5be844de5f8ef392b21296e7c5a0a2
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-09-28 21:15:33 -0700

    adding presenation 9

--- js/header.js
+++ js/header.js
@@ -26,7 +26,8 @@ class Header extends HTMLElement {
                             <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-pager-rotation-duties.html">Pager Rotation Duties</a>                            
                             <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-change-approval.html">Dangers of Change Approval</a>                            
                             <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-technology-vs-business.html">Technology vs Business</a>                            
-                            <a href="#">Presentation 8</a>
+                            <a href="https://hannahd06.github.io/personal-portfolio/web-430/delreal-security-controls.html">Security Controls</a>                            
+                            <a href="#">Presentation 9</a>
                             
                         <div>
                     </div>     
